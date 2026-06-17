8e0bb5ce5942c4c68fc658b6614a993064568ac5
Aly Khreis <akakhreis@gmail.com>
2026-06-17 10:45:34 -0400

    add slippage and comission to SimulatedExecutionHandler

--- src/execution_handler.cpp
+++ src/execution_handler.cpp
@@ -3,12 +3,17 @@
 
 
 void SimulatedExecutionHandler::execute_order(const OrderEvent& event) {
+    double close = data_->get_latest_bar(event.symbol).close;
     auto fill = std::make_unique<FillEvent>();
     fill->symbol = event.symbol;
-    fill->fill_price = data_->get_latest_bar(event.symbol).close;
+    if (event.direction == "BUY") {
+        fill->fill_price = close *(1.0 + slippage_pct_) ;
+    }else {
+        fill->fill_price = close *(1.0 - slippage_pct_) ;
+    }
     fill->direction= event.direction;
     fill->quantity = event.quantity;
-    fill->commission = 0.0;
-    fill->slippage = 0.0;
+    fill->commission = std::max(min_commission_, commission_per_share_ * fill->quantity);
+    fill->slippage = close * slippage_pct_;;
     events_ -> push(std::move(fill));
 }
--- src/execution_handler.h
+++ src/execution_handler.h
@@ -1,6 +1,7 @@
 #ifndef BACKTESTER_EXECUTION_HANDLER_H
 #define BACKTESTER_EXECUTION_HANDLER_H
 #include "data_handler.h"
+#include <algorithm>
 
 
 class ExecutionHandler {
@@ -16,10 +17,15 @@ protected:
 
 class SimulatedExecutionHandler: public ExecutionHandler {
 public:
-    SimulatedExecutionHandler(DataHandler *data, std::queue<std::unique_ptr<Event>> *events)
-    : ExecutionHandler(data, events) {};
+    SimulatedExecutionHandler(DataHandler *data, std::queue<std::unique_ptr<Event>> *events, double slippage_pct =0.0005,
+        double commission_per_share = 0.005, double min_commission = 1.0)
+    : ExecutionHandler(data, events),slippage_pct_(slippage_pct), commission_per_share_(commission_per_share),
+       min_commission_(min_commission) {};
     void execute_order(const OrderEvent& event) override;
-
+private:
+    double slippage_pct_;
+    double commission_per_share_;
+    double min_commission_;
 };
 
 
